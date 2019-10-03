29e38ec01e4d6c744a08dbb697abb549ce2fd76b
AnaPavlov <anapavlovic94@yahoo.com>
2019-10-03 20:47:59 +0200

    Updates

---
+++ precache-manifest.2d841038ef88af8c3d149633e6dbb01d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901892c7a01ac595a158b6c93f53a2ac",
+    "url": "/MillionaireQuiz/index.html"
+  },
+  {
+    "revision": "f28e50bd206646988bee",
+    "url": "/MillionaireQuiz/static/css/main.00a3a318.chunk.css"
+  },
+  {
+    "revision": "6318fa855d1747374803",
+    "url": "/MillionaireQuiz/static/js/2.7eea6073.chunk.js"
+  },
+  {
+    "revision": "f28e50bd206646988bee",
+    "url": "/MillionaireQuiz/static/js/main.99fe736d.chunk.js"
+  },
+  {
+    "revision": "5a86f9e251a77d21b380",
+    "url": "/MillionaireQuiz/static/js/runtime-main.fa28a9dd.js"
+  },
+  {
+    "revision": "b187224b9588d2d1938ff725ff760309",
+    "url": "/MillionaireQuiz/static/media/audience.b187224b.png"
+  },
+  {
+    "revision": "59c71e3156b3e17c4850983850507a9a",
+    "url": "/MillionaireQuiz/static/media/elimination.59c71e31.png"
+  },
+  {
+    "revision": "8af91c80f9bf9861e68d604669f82ac4",
+    "url": "/MillionaireQuiz/static/media/friend.8af91c80.png"
+  },
+  {
+    "revision": "75afdee587fddf1f9eda0e4b2c3e13d0",
+    "url": "/MillionaireQuiz/static/media/logo.75afdee5.png"
+  },
+  {
+    "revision": "ff325826dc509aba2a17b9781860f941",
+    "url": "/MillionaireQuiz/static/media/modal.ff325826.jpg"
+  }
+]);
