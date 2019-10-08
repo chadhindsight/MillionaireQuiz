885364f038bc2bc73c2784cbbb2b7d65cac6489c
Chad Hinds <chadrickj8@gmail.com>
2019-10-08 13:55:37 -0400

    import test renderer

--- src/test/Answers.test.js
+++ src/test/Answers.test.js
@@ -1,10 +1,3 @@
 import React, { Component } from 'react';
-//Adding the .test.js file extension makes jest automatically look for tests in the specified file.
-const TestRunner = require("jest-runner")
-import Answers from '.././components/Answers/Answers'
-
-it('should render answers component', () =>{
-    const component =  <Answers />;
-
-    expect(component).toMatchSnapshot();
-})
+// Use react-test-renderer for snapshots
+import renderer from 'react-test-renderer';
