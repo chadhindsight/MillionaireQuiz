ab4adae60778faef726092c54ed31342fb7f0123
Chad Hinds <chadrickj8@gmail.com>
2019-10-08 13:03:15 -0400

    test to ensure Answers component works

--- src/test/Answers.test.js
+++ src/test/Answers.test.js
@@ -1,1 +1,10 @@
+import React, { Component } from 'react';
 //Adding the .test.js file extension makes jest automatically look for tests in the specified file.
+const TestRunner = require("jest-runner")
+import Answers from '.././components/Answers/Answers'
+
+it('should render answers component', () =>{
+    const component =  <Answers />;
+
+    expect(component).toMatchSnapshot();
+})
